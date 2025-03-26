54085b6c676aed16eec75d9e2e384a737a02613a
ZhouKeke <zhoukk1@lenovo.com>
2025-03-26 17:49:48 +0800

    update

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -11,18 +11,19 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new();
+    // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
-
+    // basket.insert(String::from("orange"), 5);
+    basket.insert(String::from("apple"), 1);
+    basket.insert(String::from("orange"), 3);
     basket
 }
 
@@ -39,6 +40,7 @@ mod tests {
     #[test]
     fn at_least_five_fruits() {
         let basket = fruit_basket();
+
         assert!(basket.values().sum::<u32>() >= 5);
     }
 }
